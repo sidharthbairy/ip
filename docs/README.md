4b3e38940abf15be92b7215ae02de9666bdbc5ff
Sid <sidharth.bairy@gmail.com>
2026-08-20 16:29:22 +0800

    Rename chatbot from Duke to ET
    Update the Java entry point, startup banner, and user documentation to use the new chatbot name.

--- docs/README.md
+++ docs/README.md
@@ -1,4 +1,4 @@
-# Duke User Guide
+# ET User Guide
 
 // Update the title above to match the actual product name
 
---
+++ src/main/java/ET.java
@@ -0,0 +1,10 @@
+public class ET {
+    public static void main(String[] args) {
+        String banner = " _____ _____\n"
+                + "| ____|_   _|\n"
+                + "|  _|   | |\n"
+                + "| |___  | |\n"
+                + "|_____| |_|\n";
+        System.out.println(banner);
+    }
+}
